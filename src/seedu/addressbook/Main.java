93d8a5a5c8c8aa21004e2387fd2d4787ebc8664d
KEVIN CHIN <kevinchinjh@gmail.com>
2018-02-06 22:08:51 +0800

    LO-Exceptions

--- src/seedu/addressbook/Main.java
+++ src/seedu/addressbook/Main.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import seedu.addressbook.commands.Command;
 import seedu.addressbook.commands.CommandResult;
 import seedu.addressbook.commands.ExitCommand;
+import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
 import seedu.addressbook.parser.Parser;
@@ -111,6 +112,10 @@ public class Main {
             CommandResult result = command.execute();
             storage.save(addressBook);
             return result;
+        }catch(StorageOperationException eso){
+            ui.showToUser(Messages.MESSAGE_FILE_READ_ONLY);
+            start(new String[0]); //restart program
+            return new CommandResult(Messages.MESSAGE_FILE_READ_ONLY);
         } catch (Exception e) {
             ui.showToUser(e.getMessage());
             throw new RuntimeException(e);
